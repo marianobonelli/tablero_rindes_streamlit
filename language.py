9bdb09c9d162e7a43eed862e379eaf3bfa005658
Mariano Francisco Bonelli <mbonelli95@gmail.com>
2023-10-20 16:56:15 -0300

    Update language.py

--- language.py
+++ language.py
@@ -4,22 +4,26 @@ translate_dict = {
     "requested_by": {"es": "Solicitado por: ", "en": "Requested by: ", "pt": "Solicitado por: "},
     "logout": {"es": "Cerrar Sesión", "en": "Logout", "pt": "Sair"},
     "language": {"es": "Idioma", "en": "Language", "pt": "Idioma"},
+    "select_all": {"es": "Seleccionar todo", "en": "Select all", "pt": "Selecionar tudo"},
 
     # Traducciones Selector de color
     "color_palette": {"es": "Paleta de colores", "en": "Color Palette", "pt": "Paleta de Cores"},
 
     # Traducciones Area
     "area": {"es": "Área", "en": "Area", "pt": "Área"},
-    "choose_option": {"es": "elije una opción", "en": "choose an option", "pt": "escolha uma opção"},
+    "choose_option": {"es": "Elije una opción", "en": "Choose an option", "pt": "Escolha uma opção"},
 
     # Traducciones Workspace
     "workspace": {"es": "Espacio de Trabajo", "en": "Work Space", "pt": "Espaço de Trabalho"},  
 
     # Traducciones Season
-    "season": {"es": "Cmpaña", "en": "Season", "pt": "Temporada"},
+    "season": {"es": "Campaña", "en": "Season", "pt": "Temporada"},
 
     # Traducciones Farm
     "farm": {"es": "Establecimiento", "en": "Farm", "pt": "Fazenda"},
+
+    # Traducciones Field
+    "field": {"es": "Lote", "en": "Field", "pt": "Campo"},
 
     # Traducciones Cultivo
     "crop": {"es": "Cultivo", "en": "Crop", "pt": "Cultivo"},
@@ -35,6 +39,7 @@ translate_dict = {
     "adjusted_average_yield_by": {"es": "Rendimiento medio ajustado por", "en": "Adjusted Average Yield by", "pt": "Rendimento Médio Ajustado por"},
     "yield_layer": {"es": "Capa de Rinde", "en": "Yield Layer", "pt": "Camada de Rendimento"},
     "adjusted_average_yield": {"es": "Rendimiento Medio Ajustado", "en": "Adjusted Average Yield", "pt": "Rendimento Médio Ajustado"},
+    "average_yield" : {"es": "Rendimiento Medio", "en": "Average Yield", "pt": "Rendimento Médio"},
 
     # Traducciones Gráfico 2
     "weighted_yield_by": {"es": "Rendimiento Ponderado por", "en": "Weighted Yield by", "pt": "Rendimento Ponderado por"},
@@ -48,9 +53,12 @@ translate_dict = {
     "ws_field": {"es": "Espacio de Trabajo", "en": "Work Space", "pt": "Espaço de Trabalho"}, 
     "season_field": {"es": "Temporada", "en": "Season", "pt": "Temporada"},
     "farm_field": {"es": "Establecimiento", "en": "Farm", "pt": "Estabelecimento"},
-    "lot_field": {"es": "Lote", "en": "Lot", "pt": "Lote"},
+
     "crop_field": {"es": "Cultivo", "en": "Crop", "pt": "Cultivo"},
     "hybrid_variety_field": {"es": "Híbrido / Variedad", "en": "Hybrid / Variety", "pt": "Híbrido / Variedade"},
-    "adjusted_average_yield_field": {"es": "Rendimiento Medio Ajustado", "en": "Adjusted Average Yield", "pt": "Rendimento Médio Ajustado"}
+    "adjusted_average_yield_field": {"es": "Rendimiento Medio Ajustado", "en": "Adjusted Average Yield", "pt": "Rendimento Médio Ajustado"},
+
+    "select_yield_layer_warning": {"es": "Debe seleccionar una capa de rindes para continuar", "en": "You must select a yield layer to continue", "pt": "Você deve selecionar uma camada de rendimento para continuar"},
+    
 
 }
